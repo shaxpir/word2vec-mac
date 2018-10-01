579ade370f92939c2fbffa7eed1a8d7760548ae5
Benji Smith <benji@shaxpir.com>
2018-10-01 10:30:12 -0700

    wider window and more dimensions

--- build-prosecraft-vectors.sh
+++ build-prosecraft-vectors.sh
@@ -1,13 +1,13 @@
 time ./word2vec \
   -train /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word2vec-training-data.txt \
-  -output /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d150-w7.bin \
+  -output /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
   -cbow 0 \
-  -size 150 \
-  -window 7 \
-  -min-count 10 \
+  -size 300 \
+  -window 8 \
+  -min-count 15 \
   -negative 0 \
   -hs 1 \
   -sample 1e-3 \
-  -threads 12 \
+  -threads 16 \
   -binary 1
 
