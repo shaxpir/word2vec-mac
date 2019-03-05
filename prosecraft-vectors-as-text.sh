510fdc15d928ba973c73340eba931f09c789df3f
Benji Smith <benji@shaxpir.com>
2019-03-05 10:56:03 -0800

    added a shell script for dumping the bin vectors to a text file

---
+++ prosecraft-vectors-as-text.sh
@@ -0,0 +1,4 @@
+time ./readbin \
+  /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
+  > \
+  /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.txt
