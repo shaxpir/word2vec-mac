df24fb26d91d163e18a7138e82182d20a388f220
Benji Smith <benji@shaxpir.com>
2023-02-01 14:09:22 -0800

    fix: old personal username in hard-coded paths

--- build-prosecraft-vectors.sh
+++ build-prosecraft-vectors.sh
@@ -1,6 +1,6 @@
 time ./word2vec \
-  -train /Users/bsmith/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word2vec-training-data.txt \
-  -output /Users/bsmith/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
+  -train /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word2vec-training-data.txt \
+  -output /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
   -cbow 0 \
   -size 300 \
   -window 8 \
--- prosecraft-vectors-as-text.sh
+++ prosecraft-vectors-as-text.sh
@@ -1,4 +1,4 @@
 time ./readbin \
-  /Users/bsmith/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
+  /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.bin \
   > \
-  /Users/bsmith/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.txt
+  /Users/benji/dev/shaxpir/prosecraft.io/_corpus/analysis/_global/word-vectors-d300-w8.txt
